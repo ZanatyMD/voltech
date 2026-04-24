f651fc0a77cd510febdf56b6dc6b8fbb215a5cb4
Voltech User <voltech@example.com>
2026-04-25 01:52:00 +0300

    cleanup test file

--- test-logo.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const sizeOf = require('image-size');
-const content = fs.readFileSync('./src/assets/logoBase64.js', 'utf8');
-const b64 = content.split('"')[1];
-const buffer = Buffer.from(b64.split(',')[1], 'base64');
-const dimensions = sizeOf(buffer);
-console.log(dimensions);
